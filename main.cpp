131c738657b9992aeb351c76881a0501b3237bf9
Balint Szollosy <142260450+Szollobalint@users.noreply.github.com>
2024-09-23 17:18:33 +0200

    complete task

--- main.cpp
+++ main.cpp
@@ -1,36 +1,22 @@
 #include <iostream>
+#include <memory>
 
-void moveArrayContentToNewDoubleSizeArray(int *&array, int size) {
-    int *newArray = new int[size * 2];
-    for (int i = 0; i < size; i++) {
-        newArray[i] = array[i];
-    }
-    delete[] array;
-    array = newArray;
-};
-
-void printArray(int *array, int size) {
-    for (int i = 0; i < size; i++) {
-        std::cout << array[i] << " ";
-    }
-    std::cout << std::endl;
-}
 
 int main() {
-    int capacity = 5;
-    int *numbers = new int[capacity];
-    int entries = 0;
+    auto numbers = std::make_unique<int[]>(10);
+    numbers[0] = 10;
+    std::cout << numbers[0] << std::endl;
 
-    while (true) {
-        std::cout << "Please enter a number: ";
-        std::cin >> numbers[entries];
-        if (std::cin.fail()) break;
-        printArray(numbers, entries);
-        entries++;
-        if (entries > capacity) {
-            moveArrayContentToNewDoubleSizeArray(numbers, entries);
-        }
+    auto x = std::make_shared<int>();
+    *x = 10;
+
+    std::shared_ptr<int> y(x);
+
+    if (x == y) {
+        std::cout << "x is equal to y" << std::endl;
     }
+
+    std::cout << *y << std::endl;
 
     return 0;
 }
